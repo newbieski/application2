968109021920b9928e29cd28fdbbf3873f487093
newbieski <usb1usb2@naver.com>
2023-01-05 17:44:12 +0900

    현재 클랜 member 전체 조회
    ClanInfoController 추가

---
+++ src/main/java/com/application2/demo/web/ClanInfoController.java
@@ -0,0 +1,59 @@
+package com.application2.demo.web;
+
+import com.application2.demo.config.ClanConfig;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@ResponseBody
+@RestController
+public class ClanInfoController {
+    private Logger logger = LoggerFactory.getLogger(ClanInfoController.class);
+    @Autowired
+    ClanConfig clanConfig;
+    @GetMapping("/myclan/current/members")
+    public String getCurrentMembers() {
+        JSONObject response = getClansMembers();
+        JSONArray items = response.getJSONArray("items");
+        StringBuffer result = new StringBuffer();
+        for (int i = 0 ; i < items.length() ; i++) {
+            JSONObject member = items.getJSONObject(i);
+            result.append(member.get("name"));
+            result.append(" ");
+            result.append(member.get("tag"));
+            result.append("<br>");
+        }
+        return result.toString();
+    }
+
+    private JSONObject getClansMembers() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(clanConfig.getCLAN_API_TOKEN());
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        String urlString = clanConfig.getCLAN_API()+"/clans"+"/"+ clanConfig.getCLAN_TAG()+"/members";
+
+        URI uri = null;
+        try {
+            uri = new URI(urlString);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+        return new JSONObject(response.getBody().toString());
+    }
+}
