2e88521e80605ea1d1abc393983d73ae1bbcb3be
newbieski <usb1usb2@naver.com>
2023-01-10 11:33:47 +0900

    클랜리그전 결과 대시보드 추가

--- src/main/java/com/application2/demo/service/clanwarleagueattack/ClanWarLeagueAttackService.java
+++ src/main/java/com/application2/demo/service/clanwarleagueattack/ClanWarLeagueAttackService.java
@@ -2,19 +2,58 @@ package com.application2.demo.service.clanwarleagueattack;
 
 import com.application2.demo.domain.clanwarleagueattack.ClanWarLeagueAttack;
 import com.application2.demo.domain.clanwarleagueattack.ClanWarLeagueAttackRepository;
-import com.application2.demo.web.dto.ClanWarLeagueAttackDto;
+import com.application2.demo.web.dto.ClanWarLeagueAttackResonseDto;
+import com.application2.demo.web.dto.ClanWarLeagueAttackSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Service
 public class ClanWarLeagueAttackService {
+    private Logger logger = LoggerFactory.getLogger(ClanWarLeagueAttackService.class);
     private final ClanWarLeagueAttackRepository clanWarLeagueAttackRepository;
 
     @Transactional
-    public Long save(ClanWarLeagueAttackDto requestDto) {
+    public Long save(ClanWarLeagueAttackSaveRequestDto requestDto) {
         return clanWarLeagueAttackRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public ClanWarLeagueAttackResonseDto readLatest(List<String> warTags) {
+        logger.info("readLatest");
+        ClanWarLeagueAttackResonseDto responseDto = new ClanWarLeagueAttackResonseDto();
+        for (String warTag : warTags) {
+            List<ClanWarLeagueAttack> res =  clanWarLeagueAttackRepository.findAllByWarTag(warTag);
+            Collections.sort(res, new Comparator<ClanWarLeagueAttack>() {
+                @Override
+                public int compare(ClanWarLeagueAttack o1, ClanWarLeagueAttack o2) {
+                    return o2.getRegTime().compareTo(o1.getRegTime());
+                }
+            });
+            LocalDateTime base = res.get(0).getRegTime();
+            logger.info("base reg time :" + base.toString());
+            for (int i = 0 ; i < res.size() ; i++) {
+                ClanWarLeagueAttack cur = res.get(i);
+                if (cur.getRegTime().compareTo(base) != 0) {
+                    break;
+                }
+                String attackerTag = cur.getAttackerTag();
+                long total = 1, used = 0, stars = cur.getStars();
+                if (cur.getDefenderTag() != null) {
+                    used = 1;
+                }
+                responseDto.addTotal(attackerTag, total);
+                responseDto.addUsed(attackerTag, used);
+                responseDto.addStars(attackerTag, stars);
+            }
+        }
+        return responseDto;
     }
 
 }
--- src/main/java/com/application2/demo/service/dashboard/DashboardService.java
+++ src/main/java/com/application2/demo/service/dashboard/DashboardService.java
@@ -1,6 +1,9 @@
 package com.application2.demo.service.dashboard;
 
+import com.application2.demo.service.clanwarleagueattack.ClanWarLeagueAttackService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,21 +24,30 @@ public class DashboardService {
     private final MemberListService memberListService;
     private final CapitalRaidResultService capitalRaidResultService;
     private final ClanWarLeagueWarService clanWarLeagueWarService;
+    private final ClanWarLeagueAttackService clanWarLeagueAttackService;
+
+    private Logger logger = LoggerFactory.getLogger(DashboardService.class);
+
     
     public List<DashboardResponse1Dto> createDashboard1() {
+        logger.info("createDashboard1");
         List<DashboardResponse1Dto> response = new ArrayList<>();
         List<MemberListResponseDto> memberList = memberListService.readLatest();
         CapitalRaidResponseDto capitalResponse = capitalRaidResultService.getCurrentMonthSummary();
+        ClanWarLeagueAttackResonseDto clanwarleagueResponse = clanWarLeagueAttackService.readLatest(clanWarLeagueWarService.getMonthlyWarTag());
         
         HashMap<String, DashboardResponse1Dto> map = new HashMap<String, DashboardResponse1Dto>();
         for (int i = 0 ; i < memberList.size() ; i++) {
             MemberListResponseDto cur = memberList.get(i);
             String tag = cur.getTag();
             response.add(DashboardResponse1Dto.builder()
-                                    .name(cur.getName())
-                                    .tag(tag)
-                                    .capitalRaidAttackCount(capitalResponse.getAttackCount(tag))
-                                    .build());
+                            .name(cur.getName())
+                            .tag(tag)
+                            .capitalRaidAttackCount(capitalResponse.getAttackCount(tag))
+                            .clanwarlegueUsedAttackCount(clanwarleagueResponse.getUsed(tag))
+                            .clanwarlegueTotalAttackCount(clanwarleagueResponse.getTotal(tag))
+                            .clanwarleagueStars(clanwarleagueResponse.getStars(tag))
+                            .build());
         }
         
         return response;
