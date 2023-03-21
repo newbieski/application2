0d473cc80d0d6df7bce1e4e173ff32d4cff0c13f
newbieski <usb1usb2@naver.com>
2023-03-21 17:04:15 +0900

    remove subtract_min

--- src/main/java/com/application2/demo/module2/service/ApiEventService.java
+++ src/main/java/com/application2/demo/module2/service/ApiEventService.java
@@ -1,25 +1,19 @@
 package com.application2.demo.module2.service;
 
-import com.application2.demo.module1.service.clanwar.ClanWarService;
-import com.application2.demo.module2.code.ApiCode;
 import com.application2.demo.module2.domain.ApiEvent;
 import com.application2.demo.module2.domain.ApiEventRepository;
-import com.application2.demo.module2.service.dto.ApiEventHistorySaveRequestDto;
 import com.application2.demo.module2.service.dto.ApiEventSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.*;
 
 @RequiredArgsConstructor
 @Service
 public class ApiEventService {
-    private Logger logger = LoggerFactory.getLogger(ApiEventService.class);
+    private final Logger logger = LoggerFactory.getLogger(ApiEventService.class);
     private final ApiEventRepository apiEventRepository;
 
     public void saveApiEvent(ApiEventSaveRequestDto dto) {
